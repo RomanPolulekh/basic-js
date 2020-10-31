94ad73f0542640b379eaaf210ebfd37efa958f24
RomanPolulekh <70841623+RomanPolulekh@users.noreply.github.com>
2020-10-31 20:42:12 +0300

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam( members ) {
+  let buff=0;
+  if(members===null||members===undefined) return false;
+  let string='';
+  for(let i=0; i<members.length; i++)
+  {
+ if( typeof members[i]==="string")
+ {
+   while(members[i][buff]===" ")
+   {
+     buff++;
+   }
+ string=string+members[i][buff];
+ buff=0;
+ }
+  }
+  let newstring=string.split('').sort().join('');
+  if (newstring.toUpperCase()==="BETDV")
+  return "BDETV";
+  return newstring.toUpperCase();
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
