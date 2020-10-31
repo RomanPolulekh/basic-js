51e830226dea0b7aa38afd68d93be93e31b874e9
RomanPolulekh <70841623+RomanPolulekh@users.noreply.github.com>
2020-10-31 20:46:38 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,126 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(flag){
+    this.flag=flag;
+  }
+ 
+  encrypt(string, keyword) {
+    constructor(string, keyword)
+    {
+      this.string=string;
+      this.keyword=keyword;
+    }
+    let arr=[],arrKeyword=[],resultArr=[],counter=0;
+    let a=this.string.toUpperCase();
+    let b=this.keyword.toUpperCase();
+   for(let i=0;i<a.length;i++)//arr
+   {
+  if (a.charCodeAt(i)<65 || a.charCodeAt(i)>90) arr[i]=a[i]
+  else arr[i]=a.charCodeAt(i)-65;
+   }
+    for(let i=0;i<a.length;i++)//arrKeyword
+    {
+    if(counter%(b.length)===0)
+    {
+      counter=0;
+      if(a.charCodeAt(i)<65 || a.charCodeAt(i)>90)
+       {
+         arrKeyword[i]=a[i]
+        }
+      else {
+        arrKeyword[i]=b.charCodeAt(counter)-65;
+        counter++;
+      }
+    } 
+    else { 
+      if(a.charCodeAt(i)<65 || a.charCodeAt(i)>90) arrKeyword[i]=a[i]
+      else {
+        arrKeyword[i]=b.charCodeAt(counter)-65;
+        counter++;
+      }
+    }
+  }
+    for(let i=0;i<a.length;i++)
+    {
+      if(a.charCodeAt(i)>=65 && a.charCodeAt(i)<=90)
+      {
+         resultArr[i]=arr[i]+arrKeyword[i];
+         if(resultArr[i]>25) resultArr[i]=resultArr[i]-26;
+      } else
+      {
+        resultArr[i]=arr[i];
+      }
+    }
+    for(let i=0;i<a.length;i++)
+    {
+      if( typeof resultArr[i]==='number') resultArr[i]=String.fromCharCode(resultArr[i]+65)
+      
+    }
+    if(this.flag===false){
+     let buff=resultArr.join('');
+     return buff.split("").reverse().join("");
+    }
+    return  resultArr.join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  ///////////////////////////////////////////////////////////////////////
+  decrypt(string, keyword) {
+      constructor(string, keyword)
+      {
+        this.string=string;
+        this.keyword=keyword;
+      }
+      let arr=[],arrKeyword=[],resultArr=[],counter=0;
+      let a=this.string.toUpperCase();
+      let b=this.keyword.toUpperCase();
+     for(let i=0;i<a.length;i++)
+     {
+    if (a.charCodeAt(i)<65 || a.charCodeAt(i)>90) arr[i]=a[i]
+    else arr[i]=a.charCodeAt(i)-65;
+     }
+      for(let i=0;i<a.length;i++)
+      {
+      if(counter%(b.length)===0)
+      {
+        counter=0;
+        if(a.charCodeAt(i)<65 || a.charCodeAt(i)>90)
+         {
+           arrKeyword[i]=a[i]
+          }
+        else {
+          arrKeyword[i]=b.charCodeAt(counter)-65;
+          counter++;
+        }
+      } 
+      else { 
+        if(a.charCodeAt(i)<65 || a.charCodeAt(i)>90) arrKeyword[i]=a[i]
+        else {
+          arrKeyword[i]=b.charCodeAt(counter)-65;
+          counter++;
+        }
+      }
+    }
+      for(let i=0;i<a.length;i++)
+      {
+        if(a.charCodeAt(i)>=65 && a.charCodeAt(i)<=90)
+        {
+           resultArr[i]=arr[i]-arrKeyword[i];
+           if(resultArr[i]<0) resultArr[i]=resultArr[i]+26;
+        } else
+        {
+          resultArr[i]=arr[i];
+        }
+      }
+      for(let i=0;i<a.length;i++)
+      {
+        if( typeof resultArr[i]==='number') resultArr[i]=String.fromCharCode(resultArr[i]+65)
+        
+      }
+      if(this.flag===false){
+        let buff=resultArr.join('');
+        return buff.split("").reverse().join("");
+       }
+      return  resultArr.join('');
   }
 }
-
 module.exports = VigenereCipheringMachine;
