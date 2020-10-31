d8e645a654c123fffcb8ca244bce1d2432b4a663
RomanPolulekh <70841623+RomanPolulekh@users.noreply.github.com>
2020-10-31 20:44:57 +0300

    feat: add solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain:'',
   getLength() {
-    throw new CustomError('Not implemented');
+    let count=0;
+    for(let i=0; i<this.chain.length;i++)
+    {
+      if( this.chain[i]=='(')count++;
+    }
+    return count;
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+if(value==1)
+this.chain=thischain+'( '+value+' )'
+else{
+  this.chain=this.chain+'~~( '+value+' )';
+}
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+    /*let buffchain='';
+    let pos='';
+    let a=0;
+    pos=pos+String(position);
+    for(let i=0; i<this.chain.length;i++)
+    {
+      if(this.chain[i]==pos) a=i; 
+    }
+    for(let i=0; i<a-4;i++)
+    {
+      buffchain=buffchain+this.chain[i]; 
+    }
+    for(let i=a+3; i<this.chain.length;i++)
+    {
+      buffchain=buffchain+this.chain[i]; 
+    }
+    return buffchain;
+    */
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    return this.chain;
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
   }
 };
